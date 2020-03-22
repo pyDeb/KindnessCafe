6beb6c3ed06dbe80297e49a15dd60e4c3872af30
M <zeus@localhost.localdomain>
2020-03-21 20:23:27 -0400

    Created media root and News model

--- KindnessCafe/Accounts/models.py
+++ KindnessCafe/Accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model):
+    #feilds
+    title = models.CharField(max_length=75, blank=False)
+    desc = models.TextField(blank=True)
+    image = models.ImageField()
