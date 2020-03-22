471ee721a3a2dbdf49c89f1f348ccbc53a532815
M <zeus@localhost.localdomain>
2020-03-21 21:34:37 -0400

    Very first version of news.html

--- KindnessCafe/Accounts/models.py
+++ KindnessCafe/Accounts/models.py
@@ -7,3 +7,5 @@ class News(models.Model):
     title = models.CharField(max_length=75, blank=False)
     desc = models.TextField(blank=True)
     image = models.ImageField()
+    published = models.BooleanField(default=True)
+    published_date = models.DateTimeField(auto_now=True)
