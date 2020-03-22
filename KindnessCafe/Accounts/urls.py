aa77e5d6505a1086e8eb113c7bcd36526b2894cc
M <zeus@localhost.localdomain>
2020-03-21 20:53:46 -0400

    Who needs temp.html?

--- KindnessCafe/Accounts/urls.py
+++ KindnessCafe/Accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import HomePageView, SignupPageView, Index, CustomLoginView
-from class_based_auth_views.views import LogoutView
+from .views import HomePageView, SignupPageView, Index, CustomLoginView, ContactUsPageView
+#from class_based_auth_views.views import LogoutView
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('signup/', SignupPageView.as_view(), name='signup'), 
     path('login/', CustomLoginView.as_view(), name='login'), 
-    path('logout/', LogoutView.as_view(), name='logout'), 
-    path('contact/', LogoutView.as_view(), name='contact'), 
+    #path('logout/', LogoutView.as_view(), name='logout'), 
+    path('contact/', ContactUsPageView.as_view(), name='contact'), 
 ]
