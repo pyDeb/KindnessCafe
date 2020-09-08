b76a854aecc25dcb5bde65b12900a116098cc004
Iman Anooshehpour <imananoosheh@gmail.com>
2020-09-08 05:02:15 -0400

    update UI 2.0 alpha version

--- KindnessCafe/KindnessCafe/static/js/main.js
+++ KindnessCafe/KindnessCafe/static/js/main.js
@@ -3,17 +3,17 @@
 $(document).ready(function(){
   $('#kc_logos').slick({
     mobileFirst: true,
-    slidesToShow: 3,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    lazyLoad: 'ondemand',
+    centerMode: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -21,12 +21,12 @@ $(document).ready(function(){
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 376,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
