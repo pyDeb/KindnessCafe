c1f73623ca92212931b333fe826e31151faeb501
Iman Anooshehpour <imananoosheh@gmail.com>
2020-09-20 03:30:51 -0400

    Complete UI version 1

--- KindnessCafe/static/js/main.js
+++ KindnessCafe/static/js/main.js
@@ -8,20 +8,19 @@ $(document).ready(function () {
     // centerMode: true,
     // centerPadding: '200px',
     variableWidth: true,
+    dots: true,
     responsive: [{
         breakpoint: 992,
         settings: {
           slidesToShow: 5,
-          slidesToScroll: 1,
-          dots: true
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
-          dots: true
+          slidesToScroll: 1
         }
       },
       {
@@ -56,7 +55,7 @@ var btns = btnContainer.getElementsByTagName("a");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.parentNode.className += " active";
@@ -64,22 +63,67 @@ for (var i = 0; i < btns.length; i++) {
 }
 
 var urlArray = window.location.href.split("/");
-var appPathName = urlArray[urlArray.length-2];
-console.log(urlArray[urlArray.length-2]);
-if (appPathName=="news"){
+var appPathName = urlArray[urlArray.length - 2];
+//console.log(urlArray[urlArray.length - 2]);
+if (appPathName == "news") {
   btns[2].parentNode.className += " active";
-} else if (appPathName=="donation"){
-  btns[3].parentNode.className += " active";
-} else if (appPathName=="contact-us"){
-  btns[4].parentNode.className += " active";
-} else if (appPathName=="recruitment"){
+} else if (appPathName == "donation") {
   btns[5].parentNode.className += " active";
-} else{
+} else if (appPathName == "contact-us") {
+  btns[6].parentNode.className += " active";
+} else if (appPathName == "recruitment") {
+  btns[7].parentNode.className += " active";
+} else {
   btns[0].parentNode.className += " active";
 }
 
+
+if (window.location.href.split("#")[1] == "news" || appPathName == "news") {
+  document.getElementById("news-tab").click();
+}
+if (window.location.href.split("#")[1] == "events") {
+  document.getElementById("events-tab").click();
+}
+window.addEventListener('popstate', function (event) {
+  if (window.location.href.split("#")[1] == "news") {
+    document.getElementById("news-tab").click();
+  }
+  if (window.location.href.split("#")[1] == "events") {
+    document.getElementById("events-tab").click();
+  }
+});
+
+$('.copy_text').click(function (e) {
+  e.preventDefault();
+  var copyText = $(this).attr('href');
+
+  document.addEventListener('copy', function(e) {
+     e.clipboardData.setData('text/plain', copyText);
+     e.preventDefault();
+  }, true);
+
+  document.execCommand('copy');  
+  console.log('copied text : ', copyText);
+  alert('copied text: ' + copyText); 
+});
+
 setTimeout(
-  function() 
-  {
-    $.getScript("https://anoosheh.info/js/cprght.js");
+  function loadScript() {
+
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+
+    if (script.readyState) { //IE
+      script.onreadystatechange = function () {
+        if (script.readyState == "loaded" ||
+          script.readyState == "complete") {
+          script.onreadystatechange = null;
+        }
+      };
+    } else { //Others
+      script.onload = function () {};
+    }
+
+    script.src = "https://anoosheh.info/js/cprght.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
   }, 2000);
