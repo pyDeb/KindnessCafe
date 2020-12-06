edd67631d86935686aa692aa7318781dd1e13d4f
Iman Anooshehpour <imananoosheh@gmail.com>
2020-12-05 19:31:02 -0500

    Update news sharing buttons

--- KindnessCafe/News/models.py
+++ KindnessCafe/News/models.py
@@ -5,6 +5,16 @@ class News(models.Model):
     #feilds
     title = models.CharField(max_length=75, blank=False)
     desc = models.TextField(blank=True)
+
+    TAG_CATEGORY = [
+        ('NEWS', 'NEWS'),
+        ('EVENT', 'EVENT'),
+        ('LIVE', 'LIVE'),
+        ('OTHER', 'OTHER'),
+    ]
+
+    tag = models.CharField(choices=TAG_CATEGORY, max_length=5,  default='NEWS')
+
     image_1 = models.ImageField(blank=True)
     alt_1 = models.CharField(max_length=400, blank=True)
 
@@ -19,3 +29,6 @@ class News(models.Model):
 
     published = models.BooleanField(default=True)
     published_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
